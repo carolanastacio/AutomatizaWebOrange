4b95d35deb86d9eef93122789f2e16414cfc966a
Carol <carolanst@outlook.com>
2023-10-02 21:41:24 -0300

    Adicionar arquivos de projeto.

---
+++ PageObjects/DashboardPage.cs
@@ -0,0 +1,15 @@
+﻿    using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizaWebOrange.PageObjects
+{
+    public class DashboardPage
+    {
+        public static By pgDashboard = By.XPath("//*[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']");
+        public static By btRecuitment = By.XPath("//*[@href='/web/index.php/recruitment/viewRecruitmentModule']");
+    }
+}
